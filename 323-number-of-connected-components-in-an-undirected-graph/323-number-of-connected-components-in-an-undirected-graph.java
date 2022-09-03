92d144a042bde639ac188a0dfc105098f8e6f12c
Paul Abboud <33409337+abboudp@users.noreply.github.com>
2022-09-03 13:02:42 -0700

    Time: 1 ms (100.00%), Space: 42.5 MB (94.84%) - LeetHub

---
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        UnionFind graph = new UnionFind(n);
+        for (int i = 0; i < edges.length; i++) {
+            graph.union(edges[i][0], edges[i][1]);      
+        }
+        return graph.components;       
+    }
+}
+
+class UnionFind {
+    int[] root = null;
+    int[] rank = null;
+    int components;
+    
+    public UnionFind(int size) {
+        root = new int[size];
+        rank = new int[size];
+        components = size;
+        for (int i = 0; i < root.length; i++) {
+            root[i] = i;
+        }
+    }
+    
+    public int find(int x) {
+        if (x == root[x]) {
+            return x;
+        }
+        return root[x] = find(root[x]);
+    }
+    
+    public boolean union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX == rootY) {
+            return false;
+        }
+        
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                root[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                root[rootX] = rootY;
+            } else {
+                root[rootY] = rootX;
+                rank[rootX]++;
+            }
+            components--;
+        }
+        
+        return true;
+    }
+}
